009d26c49f131e49692924c1f51e6c20e90a79ec
Maurine-Jebet <maurinejebett@gmai.com>
2023-02-08 16:41:01 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,32 @@
+fun main(args: Array<String>){
+ var name = "Maurine"
+    println("Hello" +name)
+    divided(4,12)
+    addition(3,24)
+    addition(6,44)
+    addition(7,14)
+    addition(8,54)
+    name("is good at painting")
+
+
+
+
+
+
+
+
+
+}
+fun divided(num1:Int,num2:Int){
+    var result = ( num1 % num2)
+    println(result)
+}
+fun addition(num1:Int,num2:Int){
+    var result = (num1 + num2)
+    println(result)
+}
+fun name(name:String){
+    println("maureen,"+name)
+}
+
+
